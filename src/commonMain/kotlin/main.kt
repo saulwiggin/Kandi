337cc7df82659861a01dacb8036ffaa1c616c30a
saul.wiggin <saul.wiggin@retrospect.com>
2020-08-27 11:08:43 +0100

    add keyboard and mouse inputs

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,4 +1,5 @@
 import com.soywiz.klock.seconds
+import com.soywiz.korev.*
 import com.soywiz.korge.*
 import com.soywiz.korge.resources.*
 import com.soywiz.korge.tween.*
@@ -24,4 +25,58 @@ suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"
 	val sprite = sprite(runningAnimation)
 
 	sprite.playAnimationForDuration(5.seconds)
+
+	val input = views.input
+
+	addUpdater {
+		sprite.x++
+		val xy = input.mouse
+		val buttons: Int = input.mouseButtons // input mouse buttons
+
+	}
+
+	var exit = false
+
+	// input key press
+	addHrUpdater { dt ->
+		val scale = dt / 16
+		if (input.keys[Key.LEFT]) moveLeft()
+		if (input.keys[Key.RIGHT) moveRight()
+		if (input.keys[Key.UP]) moveUp()
+		if (input.keys[Key.DOWN]) moveDown()
+		if (input.keys.justPressed(Key.E)) exit = true
+		if (input.keys.justReleased(Key.ENTER)) fireWeapon("fire weapon")
+	}
+
+	mouse {
+		click {
+			fireWeapon()
+		}
+	}
+
+
+}
+
+fun fireWeapon(s: String) {
+	sendText("weapon fired")
+}
+
+fun sendText(s: String) {
+	print(s)
+}
+
+fun moveDown() {
+	TODO("Not yet implemented")
+}
+
+fun moveUp() {
+	TODO("Not yet implemented")
+}
+
+fun moveRight() {
+	TODO("Not yet implemented")
+}
+
+fun moveLeft() {
+	TODO("Not yet implemented")
 }
